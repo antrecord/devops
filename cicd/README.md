036f67acaf1ff189e593dc28dc33f51771813589
Anton <anton@ksenofontov>
2026-03-30 16:37:11 +0000

    lab 11

---
+++ cicd/README.md
@@ -0,0 +1,9 @@
+Лабораторная работа №11
+
+.github/workflows - пайплайны
+
+/server - приложение + dockerfile + тесты
+
+/server-k8s-manifests - манифест кубера
+
+requirements.txt - зависимости для питона
---
+++ cicd/server/application.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+class TestMe():
+    def take_five(self):
+        return 5
+    def port(self):
+        return PORT
+
+if __name__ == '__main__':
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("",PORT), Handler) as http:
+        print("servingatport", PORT)
+        http.serve_forever()
